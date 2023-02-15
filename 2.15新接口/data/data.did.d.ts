bd6e97777672e465766b798788bbac00155a5c52
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2023-02-15 11:12:12 +0800

    2.15 新接口

---
+++ 2.15新接口/data/data.did.d.ts
@@ -0,0 +1,190 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export type DataSource = { 'Soil' : null } |
+  { 'Vegetation' : null };
+export type DataSource__1 = { 'Soil' : null } |
+  { 'Vegetation' : null };
+export type Error = { 'AlreadyHaveThisReserve' : null } |
+  { 'TimeError' : null } |
+  { 'SomeThingWrong' : null } |
+  { 'ReserveNotExist' : null };
+export interface FieldReserve {
+  'soc_stock_amount' : string,
+  'std_amount' : string,
+  'soc_density_amount' : string,
+  'fieldName' : string,
+}
+export interface FieldReserve__1 {
+  'soc_stock_amount' : string,
+  'std_amount' : string,
+  'soc_density_amount' : string,
+  'fieldName' : string,
+}
+export interface RESERVES {
+  'id' : bigint,
+  'year_end' : bigint,
+  'upload_count' : bigint,
+  'name' : string,
+  'soc_stock_amount' : string,
+  'std_amount' : string,
+  'regionLevel' : RegionLevel,
+  'year_start' : bigint,
+  'soc_density_amount' : string,
+}
+export interface RESERVESContainField {
+  'id' : bigint,
+  'year_end' : bigint,
+  'upload_count' : bigint,
+  'fieldReserves' : Array<FieldReserve>,
+  'name' : string,
+  'regionLevel' : RegionLevel,
+  'year_start' : bigint,
+}
+export type RegionLevel = { 'City' : null } |
+  { 'Country_and_Region' : null } |
+  { 'Province' : null };
+export type RegionLevel__1 = { 'City' : null } |
+  { 'Country_and_Region' : null } |
+  { 'Province' : null };
+export type Result = { 'ok' : boolean } |
+  { 'err' : Error };
+export interface SumRESERVES {
+  'id' : bigint,
+  'year_end' : bigint,
+  'upload_count' : bigint,
+  'name' : string,
+  'regionLevel' : RegionLevel,
+  'sumSoc' : string,
+  'year_start' : bigint,
+}
+export interface SumRESERVESContainField {
+  'id' : bigint,
+  'year_end' : bigint,
+  'upload_count' : bigint,
+  'fieldReserves' : Array<[string, string]>,
+  'name' : string,
+  'regionLevel' : RegionLevel,
+  'year_start' : bigint,
+}
+export interface UploadFieldArgs {
+  'id' : bigint,
+  'year_end' : bigint,
+  'dataSource' : DataSource,
+  'upload_count' : bigint,
+  'fieldReserves' : Array<FieldReserve>,
+  'name' : string,
+  'regionLevel' : RegionLevel,
+  'year_start' : bigint,
+}
+export interface data {
+  'getAllAverageFieldData' : ActorMethod<
+    [DataSource__1],
+    Array<RESERVESContainField>,
+  >,
+  'getAllAverageSumFieldData' : ActorMethod<[], Array<SumRESERVESContainField>>,
+  'getAllFieldData' : ActorMethod<[DataSource__1], Array<RESERVESContainField>>,
+  'getAllSumFieldData' : ActorMethod<[], Array<SumRESERVESContainField>>,
+  'getAllSumSoc' : ActorMethod<[], Array<SumRESERVES>>,
+  'getAverageFieldData' : ActorMethod<
+    [DataSource__1, bigint, bigint],
+    [] | [RESERVESContainField],
+  >,
+  'getCertainFieldAverageData' : ActorMethod<
+    [DataSource__1, bigint, bigint, string],
+    [] | [FieldReserve__1],
+  >,
+  'getCertainFieldData' : ActorMethod<
+    [DataSource__1, bigint, bigint, string],
+    [] | [FieldReserve__1],
+  >,
+  'getCertain_Id_Time_Data' : ActorMethod<
+    [DataSource__1, bigint, bigint],
+    [] | [RESERVES],
+  >,
+  'getCertain_Id_Time_Field_sumAvergaeData' : ActorMethod<
+    [bigint, bigint, string],
+    [] | [string],
+  >,
+  'getCertain_Id_Time_Field_sumData' : ActorMethod<
+    [bigint, bigint, string],
+    [] | [string],
+  >,
+  'getCertain_Id_Time_SumSoc' : ActorMethod<
+    [bigint, bigint],
+    [] | [SumRESERVES],
+  >,
+  'getCertain_Id_Time_sumAverageFieldData' : ActorMethod<
+    [bigint, bigint],
+    [] | [SumRESERVESContainField],
+  >,
+  'getCertain_Id_Time_sumFieldData' : ActorMethod<
+    [bigint, bigint],
+    [] | [SumRESERVESContainField],
+  >,
+  'getCertain_RegionLevel_AverageFieldData' : ActorMethod<
+    [DataSource__1, RegionLevel__1],
+    Array<RESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_FieldData' : ActorMethod<
+    [DataSource__1, RegionLevel__1],
+    Array<RESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_SumSoc' : ActorMethod<
+    [RegionLevel__1],
+    Array<SumRESERVES>,
+  >,
+  'getCertain_RegionLevel_Time_AverageFieldData' : ActorMethod<
+    [DataSource__1, RegionLevel__1, bigint],
+    Array<RESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_Time_Data' : ActorMethod<
+    [DataSource__1, RegionLevel__1, bigint],
+    Array<RESERVES>,
+  >,
+  'getCertain_RegionLevel_Time_FieldData' : ActorMethod<
+    [DataSource__1, RegionLevel__1, bigint],
+    Array<RESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_Time_SumSoc' : ActorMethod<
+    [RegionLevel__1, bigint],
+    Array<SumRESERVES>,
+  >,
+  'getCertain_RegionLevel_Time_sumAverageFieldData' : ActorMethod<
+    [RegionLevel__1, bigint],
+    Array<SumRESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_Time_sumFieldData' : ActorMethod<
+    [RegionLevel__1, bigint],
+    Array<SumRESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_sumAverageFiledData' : ActorMethod<
+    [RegionLevel__1],
+    Array<SumRESERVESContainField>,
+  >,
+  'getCertain_RegionLevel_sumFiledData' : ActorMethod<
+    [RegionLevel__1],
+    Array<SumRESERVESContainField>,
+  >,
+  'getCertanin_RegionLevel_Data' : ActorMethod<
+    [DataSource__1, RegionLevel__1],
+    Array<RESERVES>,
+  >,
+  'getFieldData' : ActorMethod<
+    [DataSource__1, bigint, bigint],
+    [] | [RESERVESContainField],
+  >,
+  'getIdToNameMap' : ActorMethod<[], Array<[bigint, string]>>,
+  'getNameToIdMap' : ActorMethod<[], Array<[string, bigint]>>,
+  'idToName' : ActorMethod<[bigint], [] | [string]>,
+  'nameToId' : ActorMethod<[string], [] | [bigint]>,
+  'newNameId' : ActorMethod<[string], bigint>,
+  'uploadAverageFieldData' : ActorMethod<[UploadFieldArgs], Result>,
+  'uploadFieldData' : ActorMethod<[UploadFieldArgs], Result>,
+  'uploadSoilData' : ActorMethod<[RESERVES], Result>,
+  'uploadSumAvergaeFieldData' : ActorMethod<[SumRESERVESContainField], Result>,
+  'uploadSumData' : ActorMethod<[SumRESERVES], Result>,
+  'uploadSumFieldData' : ActorMethod<[SumRESERVESContainField], Result>,
+  'uploadVegetationData' : ActorMethod<[RESERVES], Result>,
+}
+export interface _SERVICE extends data {}
---
+++ 2.15新接口/data/data.did.js
@@ -0,0 +1,246 @@
+export const idlFactory = ({ IDL }) => {
+  const DataSource__1 = IDL.Variant({
+    'Soil' : IDL.Null,
+    'Vegetation' : IDL.Null,
+  });
+  const FieldReserve = IDL.Record({
+    'soc_stock_amount' : IDL.Text,
+    'std_amount' : IDL.Text,
+    'soc_density_amount' : IDL.Text,
+    'fieldName' : IDL.Text,
+  });
+  const RegionLevel = IDL.Variant({
+    'City' : IDL.Null,
+    'Country_and_Region' : IDL.Null,
+    'Province' : IDL.Null,
+  });
+  const RESERVESContainField = IDL.Record({
+    'id' : IDL.Nat,
+    'year_end' : IDL.Nat,
+    'upload_count' : IDL.Nat,
+    'fieldReserves' : IDL.Vec(FieldReserve),
+    'name' : IDL.Text,
+    'regionLevel' : RegionLevel,
+    'year_start' : IDL.Nat,
+  });
+  const SumRESERVESContainField = IDL.Record({
+    'id' : IDL.Nat,
+    'year_end' : IDL.Nat,
+    'upload_count' : IDL.Nat,
+    'fieldReserves' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+    'name' : IDL.Text,
+    'regionLevel' : RegionLevel,
+    'year_start' : IDL.Nat,
+  });
+  const SumRESERVES = IDL.Record({
+    'id' : IDL.Nat,
+    'year_end' : IDL.Nat,
+    'upload_count' : IDL.Nat,
+    'name' : IDL.Text,
+    'regionLevel' : RegionLevel,
+    'sumSoc' : IDL.Text,
+    'year_start' : IDL.Nat,
+  });
+  const FieldReserve__1 = IDL.Record({
+    'soc_stock_amount' : IDL.Text,
+    'std_amount' : IDL.Text,
+    'soc_density_amount' : IDL.Text,
+    'fieldName' : IDL.Text,
+  });
+  const RESERVES = IDL.Record({
+    'id' : IDL.Nat,
+    'year_end' : IDL.Nat,
+    'upload_count' : IDL.Nat,
+    'name' : IDL.Text,
+    'soc_stock_amount' : IDL.Text,
+    'std_amount' : IDL.Text,
+    'regionLevel' : RegionLevel,
+    'year_start' : IDL.Nat,
+    'soc_density_amount' : IDL.Text,
+  });
+  const RegionLevel__1 = IDL.Variant({
+    'City' : IDL.Null,
+    'Country_and_Region' : IDL.Null,
+    'Province' : IDL.Null,
+  });
+  const DataSource = IDL.Variant({
+    'Soil' : IDL.Null,
+    'Vegetation' : IDL.Null,
+  });
+  const UploadFieldArgs = IDL.Record({
+    'id' : IDL.Nat,
+    'year_end' : IDL.Nat,
+    'dataSource' : DataSource,
+    'upload_count' : IDL.Nat,
+    'fieldReserves' : IDL.Vec(FieldReserve),
+    'name' : IDL.Text,
+    'regionLevel' : RegionLevel,
+    'year_start' : IDL.Nat,
+  });
+  const Error = IDL.Variant({
+    'AlreadyHaveThisReserve' : IDL.Null,
+    'TimeError' : IDL.Null,
+    'SomeThingWrong' : IDL.Null,
+    'ReserveNotExist' : IDL.Null,
+  });
+  const Result = IDL.Variant({ 'ok' : IDL.Bool, 'err' : Error });
+  const data = IDL.Service({
+    'getAllAverageFieldData' : IDL.Func(
+        [DataSource__1],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getAllAverageSumFieldData' : IDL.Func(
+        [],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getAllFieldData' : IDL.Func(
+        [DataSource__1],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getAllSumFieldData' : IDL.Func(
+        [],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getAllSumSoc' : IDL.Func([], [IDL.Vec(SumRESERVES)], ['query']),
+    'getAverageFieldData' : IDL.Func(
+        [DataSource__1, IDL.Nat, IDL.Nat],
+        [IDL.Opt(RESERVESContainField)],
+        ['query'],
+      ),
+    'getCertainFieldAverageData' : IDL.Func(
+        [DataSource__1, IDL.Nat, IDL.Nat, IDL.Text],
+        [IDL.Opt(FieldReserve__1)],
+        ['query'],
+      ),
+    'getCertainFieldData' : IDL.Func(
+        [DataSource__1, IDL.Nat, IDL.Nat, IDL.Text],
+        [IDL.Opt(FieldReserve__1)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_Data' : IDL.Func(
+        [DataSource__1, IDL.Nat, IDL.Nat],
+        [IDL.Opt(RESERVES)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_Field_sumAvergaeData' : IDL.Func(
+        [IDL.Nat, IDL.Nat, IDL.Text],
+        [IDL.Opt(IDL.Text)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_Field_sumData' : IDL.Func(
+        [IDL.Nat, IDL.Nat, IDL.Text],
+        [IDL.Opt(IDL.Text)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_SumSoc' : IDL.Func(
+        [IDL.Nat, IDL.Nat],
+        [IDL.Opt(SumRESERVES)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_sumAverageFieldData' : IDL.Func(
+        [IDL.Nat, IDL.Nat],
+        [IDL.Opt(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_Id_Time_sumFieldData' : IDL.Func(
+        [IDL.Nat, IDL.Nat],
+        [IDL.Opt(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_AverageFieldData' : IDL.Func(
+        [DataSource__1, RegionLevel__1],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_FieldData' : IDL.Func(
+        [DataSource__1, RegionLevel__1],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_SumSoc' : IDL.Func(
+        [RegionLevel__1],
+        [IDL.Vec(SumRESERVES)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_AverageFieldData' : IDL.Func(
+        [DataSource__1, RegionLevel__1, IDL.Nat],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_Data' : IDL.Func(
+        [DataSource__1, RegionLevel__1, IDL.Nat],
+        [IDL.Vec(RESERVES)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_FieldData' : IDL.Func(
+        [DataSource__1, RegionLevel__1, IDL.Nat],
+        [IDL.Vec(RESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_SumSoc' : IDL.Func(
+        [RegionLevel__1, IDL.Nat],
+        [IDL.Vec(SumRESERVES)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_sumAverageFieldData' : IDL.Func(
+        [RegionLevel__1, IDL.Nat],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_Time_sumFieldData' : IDL.Func(
+        [RegionLevel__1, IDL.Nat],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_sumAverageFiledData' : IDL.Func(
+        [RegionLevel__1],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertain_RegionLevel_sumFiledData' : IDL.Func(
+        [RegionLevel__1],
+        [IDL.Vec(SumRESERVESContainField)],
+        ['query'],
+      ),
+    'getCertanin_RegionLevel_Data' : IDL.Func(
+        [DataSource__1, RegionLevel__1],
+        [IDL.Vec(RESERVES)],
+        ['query'],
+      ),
+    'getFieldData' : IDL.Func(
+        [DataSource__1, IDL.Nat, IDL.Nat],
+        [IDL.Opt(RESERVESContainField)],
+        ['query'],
+      ),
+    'getIdToNameMap' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Nat, IDL.Text))],
+        ['query'],
+      ),
+    'getNameToIdMap' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Nat))],
+        ['query'],
+      ),
+    'idToName' : IDL.Func([IDL.Nat], [IDL.Opt(IDL.Text)], ['query']),
+    'nameToId' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Nat)], ['query']),
+    'newNameId' : IDL.Func([IDL.Text], [IDL.Nat], []),
+    'uploadAverageFieldData' : IDL.Func([UploadFieldArgs], [Result], []),
+    'uploadFieldData' : IDL.Func([UploadFieldArgs], [Result], []),
+    'uploadSoilData' : IDL.Func([RESERVES], [Result], []),
+    'uploadSumAvergaeFieldData' : IDL.Func(
+        [SumRESERVESContainField],
+        [Result],
+        [],
+      ),
+    'uploadSumData' : IDL.Func([SumRESERVES], [Result], []),
+    'uploadSumFieldData' : IDL.Func([SumRESERVESContainField], [Result], []),
+    'uploadVegetationData' : IDL.Func([RESERVES], [Result], []),
+  });
+  return data;
+};
+export const init = ({ IDL }) => { return []; };
